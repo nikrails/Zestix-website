5c130d13f0a59cb4344c23b3ce7d0ed0243ee922
Zestics <Zestics>
2016-07-02 11:07:10 +0530

    First commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(window.Startup))]
+namespace window
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Projects/ConsoleApplication1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+           {
+            int a;
+            Console.WriteLine("Enter numbers");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Youhave entered"+a);
+        }
+    }
+}
